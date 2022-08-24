7f2d5ea50d46c73c3d8dc9a32bb129b1ab9c6789
Zach Goethel <13502112+zgoethel@users.noreply.github.com>
2022-08-23 20:00:45 -0500

    Misc floating project changes

--- Jibini.SharedBase.Components/Util/Extensions/ComponentRenderingExtensions.cs
+++ Jibini.SharedBase.Components/Util/Extensions/ComponentRenderingExtensions.cs
@@ -19,14 +19,13 @@ public static class ComponentRenderingExtensions
     /// Creates a duplicate component to render it to HTML, returning the outer
     /// HTML content. Components do not need to implement the method.
     /// </summary>
-    public static string? ToHtml<T>(this IRenderable<T> it)
+    public static string ToHtml<T>(this IRenderable<T> it)
         where T : IComponent
     {
-        if (it is null)
-            return null;
-
         var renderer = new ComponentRenderer<T>();
-        var fields = typeof(T).GetProperties();
+        var fields = typeof(T).GetProperties()
+            .ToList()
+            .FindAll((it) => it.GetCustomAttributes(typeof(ParameterAttribute), true).Any());
         var paramInfo = Expression.Parameter(typeof(T));
 
         // Copy every field which can be written via renderer setter call
@@ -35,7 +34,7 @@ public static class ComponentRenderingExtensions
             var value = field.GetValue(it);
 
             // Blazor Templater does not support setting parameters to null
-            if (value is not null && field.CanWrite)
+            if (value is not null)
             {
                 var access = Expression.MakeMemberAccess(paramInfo, field);
                 var expr = Expression.Lambda<Func<T, object?>>(access, paramInfo);
