8dc9622fbf379604f7f71c971f6ba7be6b24377c
Zach Goethel <13502112+zgoethel@users.noreply.github.com>
2023-08-15 22:02:44 -0500

    Comment consistency

--- Jibini.SharedBase.LibUtil/Language/TokenStream.cs
+++ Jibini.SharedBase.LibUtil/Language/TokenStream.cs
@@ -1,34 +1,36 @@
 ﻿namespace Jibini.SharedBase.Util;
 
-/*
- * Encapsulates a source file and also the current cursor state of the lexer-
- * parser. It supports a single token of lookahead, which allows polling a
- * "current" token into memory then peeking the "next" token off the top of the
- * token stream.
- */
+/// <summary>
+/// Encapsulates a source file and also the current cursor state of the lexer-
+/// parser.It supports a single token of lookahead, which allows polling a
+/// "current" token into memory then peeking the "next" token off the top of the
+/// token stream.
+/// </summary>
 public class TokenStream
 {
     public Fsa Grammar { get; set; } = new();
     public string Source { get; set; } = "";
-    /*
-     * Cursor position can be reset, but take care to anchor to token starts, to
-     * remain within source, and to clear the cached "next token."
-     */
+
+    /// <summary>
+    /// Cursor position can be reset, but take care to anchor to token starts, to
+    /// remain within source, and to clear the cached "next token."
+    /// </summary>
     public int Offset { get; private set; }
 
-    /*
-     * Seeks the source cursor to the specified offset, clearing the cached
-     * token to ensure it gets re-evaluated.
-     */
+    /// <summary>
+    /// Seeks the source cursor to the specified offset, clearing the cached
+    /// token to ensure it gets re-evaluated.
+    /// </summary>
+    /// <param name="offset"></param>
     public void Seek(int offset)
     {
         Offset = offset;
         nextToken = null;
     }
 
-    /*
-     * Allows inspection of the current source position in the debugger.
-     */
+    /// <summary>
+    /// Allows inspection of the current source position in the debugger.
+    /// </summary>
     public string Remaining => Source.Substring(Offset);
 
     // Cached "next token" value re-evaluated when peeking
@@ -45,16 +47,16 @@ public class TokenStream
         }
     }
 
-    /*
-     * String content matched by the previous token, which can be further
-     * interpreted for literals or identifiers.
-     */
+    /// <summary>
+    /// String content matched by the previous token, which can be further
+    /// interpreted for literals or identifiers.
+    /// </summary>
     public string Text { get; private set; } = "";
 
-    /*
-     * Searches the grammar for a token matching the characters found at the
-     * source's cursor offset and onwards. Whitespace will be ignored.
-     */
+    /// <summary>
+    /// Searches the grammar for a token matching the characters found at the
+    /// source's cursor offset and onwards. Whitespace will be ignored.
+    /// </summary>
     public int Peek()
     {
         if (Offset >= Source.Length)
@@ -75,12 +77,12 @@ public class TokenStream
         return accepted;
     }
 
-    /*
-     * Searches the grammar for a token matching the characters found at the
-     * source's cursor offset and onwards. Whitespace will be ignored. After
-     * calling, the matched length of text will be consumed from source (and the
-     * cursor will move forward by that number of characters).
-     */ 
+    /// <summary>
+    /// Searches the grammar for a token matching the characters found at the
+    /// source's cursor offset and onwards. Whitespace will be ignored. After
+    /// calling, the matched length of text will be consumed from source (and the
+    /// cursor will move forward by that number of characters).
+    /// </summary>
     public int Poll()
     {
         var token = Next;
